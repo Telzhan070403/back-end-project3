f3fd73f7f62728ad12e6f2b5e2aafcf0d357a25b
Telzhan070403 <82749798+Telzhan070403@users.noreply.github.com>
2022-05-05 21:20:30 +0600

    first commit

---
+++ last/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+from .views import EmailAttachementView, RegisterUser
+
+urlpatterns = [
+    path('', views.kazakstan, name='kazakhstan'),
+    path('Kazakhstan/', views.home, name='index'),
+    path('send/', EmailAttachementView.as_view(), name='emailattachment'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+]
